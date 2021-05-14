c9072864c04118296784b250ac4f35654d173b51
wangzhipeng <11>
2021-05-14 17:15:09 +0800

    update

--- run/test.py
+++ run/test.py
@@ -11,25 +11,25 @@ curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 from case.contract import m
-# from case.message import community
-# from case.image_review import R
-# from case.block import Block
+from case.message import community
+from case.image_review import R
+from case.block import Block
 warnings.filterwarnings("ignore")
 
 
-# class TestCase_message(unittest.TestCase):
-#     '''内容管理模块'''
-#     @classmethod
-#     def setUpClass(cls):
-#         log().info('******************** 内容管理测试开始 ********************')
-#     @classmethod
-#     def tearDownClass(cls):
-#         log().info('********************  测试结束  ********************')
-#
-#     def testcase01(self):
-#         '''社区官方发布资讯'''
-#         result = community.send()
-#         self.assertIn("成功", result, "测试失败")
+class TestCase_message(unittest.TestCase):
+    '''内容管理模块'''
+    @classmethod
+    def setUpClass(cls):
+        log().info('******************** 内容管理测试开始 ********************')
+    @classmethod
+    def tearDownClass(cls):
+        log().info('********************  测试结束  ********************')
+
+    def testcase01(self):
+        '''社区官方发布资讯'''
+        result = community.send()
+        self.assertIn("成功", result, "测试失败")
 
 class TestCase_image(unittest.TestCase):
     '''舆论管理'''
@@ -45,27 +45,27 @@ class TestCase_image(unittest.TestCase):
         result=m.check()
         self.assertIn("操作成功",result,"测试失败")
 
-    # def testcase02(self):
-    #     '''图片审核'''
-    #     result=m.review()
-    #     self.assertIn('通过',result,'测试失败')
-#
-# class TestCase_contract(unittest.TestCase):
-#     '''用户管理'''
-#     @classmethod
-#     def setUpClass(cls):
-#         log().info('******************** OA测试开始 ********************')
-#     @classmethod
-#     def tearDownClass(cls):
-#         log().info('********************  测试结束  ********************')
-#
-#     def testcase01(self):
-#         '''封禁用户'''
-#         result=Block.block()
-#         self.assertIn("成功",result,"测试失败")
-#
-#     def testcase02(self):
-#         pass
+    def testcase02(self):
+        '''图片审核'''
+        result=R.review()
+        self.assertIn('通过',result,'测试失败')
+
+class TestCase_contract(unittest.TestCase):
+    '''用户管理'''
+    @classmethod
+    def setUpClass(cls):
+        log().info('******************** OA测试开始 ********************')
+    @classmethod
+    def tearDownClass(cls):
+        log().info('********************  测试结束  ********************')
+
+    def testcase01(self):
+        '''封禁用户'''
+        result=Block.block()
+        self.assertIn("成功",result,"测试失败")
+
+    def testcase02(self):
+        pass
 
 
 if __name__=='__main__':
